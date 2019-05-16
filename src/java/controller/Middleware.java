0d82faecae739dc805dd6c96846de8f921db3030
Unknown <josselin.pennors@hotmail.fr>
2019-05-16 18:21:37 +0200

    Create middleware for routes

---
+++ src/java/controller/Middleware.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.User;
+
+/**
+ *
+ * @author Josselin
+ */
+public class Middleware {
+
+    static public boolean isConnected(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") == null){
+            return false;
+        }
+        return true;
+    }
+    
+    static public boolean isAdmin(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null){
+            User user = (User) session.getAttribute("user");
+            if(user.getIs_admin()){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static public User getLoggedUser(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null){
+            return (User) session.getAttribute("user");
+        }
+        return null;
+    }
+}
