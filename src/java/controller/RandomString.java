1df7dd6c64e56f4e3e17c2920e4f276e9083277d
Unknown <josselin.pennors@hotmail.fr>
2019-06-11 23:02:40 +0200

    Add class to get create a random string

---
+++ src/java/controller/RandomString.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Josselin
+ */
+ 
+import java.util.Random;
+ 
+public class RandomString {
+ 
+    private static final String CHAR_LIST = 
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    private static final int RANDOM_STRING_LENGTH = 10;
+     
+    /**
+     * This method generates random string
+     * @return
+     */
+    public static String generateRandomString(){
+         
+        StringBuffer randStr = new StringBuffer();
+        for(int i=0; i<RANDOM_STRING_LENGTH; i++){
+            int number = getRandomNumber();
+            char ch = CHAR_LIST.charAt(number);
+            randStr.append(ch);
+        }
+        return randStr.toString();
+    }
+     
+    /**
+     * This method generates random numbers
+     * @return int
+     */
+    private static int getRandomNumber() {
+        int randomInt = 0;
+        Random randomGenerator = new Random();
+        randomInt = randomGenerator.nextInt(CHAR_LIST.length());
+        if (randomInt - 1 == -1) {
+            return randomInt;
+        } else {
+            return randomInt - 1;
+        }
+    }
+     
+}
